edfd7efa2d5eae4f300c3e94b21ebb09969e9de5
Chirantan Banik <banikchirantan16@gmail.com>
2023-12-01 11:49:01 +0530

    Store

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit"
+import authSlice from "./authSlice"
+
 const store = configureStore({
     reducer: {
+        auth: authSlice,
         
     }
 })
