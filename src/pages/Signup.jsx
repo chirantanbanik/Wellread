9b65fde83bac1110692144425dd964b96d8b2122
Chirantan Banik <banikchirantan16@gmail.com>
2023-12-01 11:47:36 +0530

    Signup

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Signup as SignupComponent} from "../components"
+
+function Signup() {
+  return (
+    <div
+    className='py-8'>
+        <SignupComponent/>
+    </div>
+  )
+}
+
+export default Signup
